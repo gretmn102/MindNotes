cd515a90ca9fcd44aa305c8d3a51b5669fa39f49
gretmn102 <erlgreywatson@gmail.com>
2020-10-14 12:20:58 +0300

    many changes

--- RandomNotes.fsx
+++ RandomNotes.fsx
@@ -9,7 +9,7 @@
 open FsharpMyExtension
 open FsharpMyExtension.Either
 
-type T = { DateTime:System.DateTime option; Tags:string list; Text:string }
+type Note = { DateTime:System.DateTime option; Tags:string list; Text:string }
 
 open FParsec
 type 'a Parser = Parser<'a,unit>
@@ -33,16 +33,18 @@ let pdatetime2 : _ Parser =
         (pint32 .>> pchar '.')
         (pint32 .>> pchar '.')
         (pint32 .>> skipSpaces')
-        (opt (tuple2
-                (pint32 .>> pchar ':')
-                (pint32 .>> pchar ':' .>>. pint32))
+        (opt (tuple3
+                pint32
+                (pchar ':' >>. pint32)
+                (opt (pchar ':' >>. pint32)))
             )
         (fun day month year time ->
             match time with
-            | Some(hour, (min, sec)) ->
+            | Some(hour, min, sec) ->
+                let sec = sec |> Option.defaultValue 0
                 System.DateTime(year, month, day, hour, min, sec)
             | None -> System.DateTime(year, month, day))
-let text : _ Parser =
+let ptext : _ Parser =
     many1Strings
         (
             notFollowedBy (pstring "***" .>> (skipNewline <|> eof))
@@ -50,7 +52,7 @@ let text : _ Parser =
         )
 let textTest () =
     let test str =
-        match run text str with
+        match run ptext str with
         | Success(x, _, _) -> Right x
         | Failure(x, _, _) -> Left x
     [
@@ -74,7 +76,7 @@ let pdatetimeStrange : _ Parser =
             failFatally e.Message
 
 // run pdatetimeStrange "\u200e6 \u200eдекабря \u200e2018 \u200eг., \u200f\u200e14:17:27"
-let p : _ Parser =
+let pnote ptext : _ Parser =
     let tags =
         // run (notFollowedByL (pchar '#' >>. satisfy (isAnyOf " \n")) "# <space|\\n>") "# some"
         let p = pstring "//" >>. skipSpaces'
@@ -84,7 +86,7 @@ let p : _ Parser =
         (opt (pdatetimeStrange <|> pdatetime2 .>> skipNewline))
         // (opt (opt (pstring "//" >>. skipSpaces') >>? (tags1 .>> skipNewline)))
         (opt tags)
-        text
+        ptext
         (fun datetime tags text ->
             { DateTime = datetime
               Tags = tags |> Option.defaultValue []
@@ -96,42 +98,43 @@ let test () =
             "#сюжет"
             "— А ты мне что скажешь? — нетерпеливо спросил царь."
         ] |> String.concat "\n"
-    run p str
+    run (pnote ptext) str
 let start : _ Parser =
     let sep = pstring "***" .>> skipNewline
-    opt sep
-    >>. sepEndBy1 p sep
+    optional sep
+    >>. sepEndBy1 (pnote ptext) sep
     .>> eof
 // run (opt (pstring "//" >>. skipSpaces') >>? tags .>> skipNewline) "Настольные игры.\n"
 // run text "Настольные игры."
 let startOnFile path =
     match runParserOnFile start () path System.Text.Encoding.UTF8 with
-    | Success(xs, _, _) -> xs
-    | Failure(x, _, _) -> failwithf "%s" x
+    | Success(notes, _, _) -> notes
+    | Failure(errMsg, _, _) -> failwithf "%s" errMsg
+let parseNoteOnFile notePath =
+    match runParserOnFile (spaces >>. pnote (manySatisfy (fun _ -> true))) () notePath System.Text.Encoding.UTF8 with
+    | Success(note, _, _) -> Right note
+    | Failure(errMsg, _, _) -> Left errMsg
 
-let print =
-    let show (x:T) =
-        let datetime =
-            x.DateTime
-            |> Option.map (fun x -> x.ToString() |> sprintf "%s\n")
-            |> Option.defaultValue ""
-        let tags =
-            match x.Tags with
-            | [] -> ""
-            | xs ->
-                xs |> List.map (sprintf "#%s")
-                |> String.concat " " |> sprintf "%s\n"
-        sprintf "%s%s%s" datetime tags (x.Text.TrimEnd())
-    List.map show >> String.concat "\n***\n"
+let notePrint (x:Note) =
+    let datetime =
+        x.DateTime
+        |> Option.map (fun x -> x.ToString() |> sprintf "%s\n")
+        |> Option.defaultValue ""
+    let tags =
+        match x.Tags with
+        | [] -> ""
+        | xs ->
+            xs |> List.map (sprintf "#%s")
+            |> String.concat " " |> sprintf "%s\n"
+    sprintf "%s%s%s" datetime tags (x.Text.TrimEnd())
+let notesPrint =
+    List.map notePrint >> String.concat "\n***\n"
 
-let test2 randomNotesPath =
-    match runParserOnFile start () randomNotesPath System.Text.Encoding.UTF8 with
-    | Success(x, _, _) ->
-        print x
-        |> uncurry System.IO.File.WriteAllText "output\\output.txt"
-        x
-    | x -> failwithf "%A" x
-    // System.DateTime.Parse("28 апреля 2017 г., 22:25:40") |> fun x -> x.ToString() |> Clipboard.setText
+let test2 notesPath =
+    startOnFile notesPath
+    |> notesPrint
+    |> uncurry System.IO.File.WriteAllText "output\\output.txt"
+
 // test2 "notes.txt"
 
 let allTags =
@@ -228,12 +231,12 @@ let writeToDiary randomNotesPath diaryDbPath =
     CoreDiary.Ser.save diaryDbPath db
 
     System.IO.File.Copy(randomNotesPath, backupPath randomNotesPath, true)
-    print randomNotes
+    notesPrint randomNotes
     |> uncurry System.IO.File.WriteAllText randomNotesPath
 
 
 let split randomNotesPath tagsToPaths =
-    let randomNotes : T list = startOnFile randomNotesPath
+    let randomNotes : Note list = startOnFile randomNotesPath
 
     let splitNotOptimize tagsToPaths randomNotes =
         tagsToPaths
@@ -286,13 +289,13 @@ let split randomNotesPath tagsToPaths =
             if not <| List.isEmpty notes then
                 let backupPath = System.IO.Path.ChangeExtension(path, ".bak")
                 System.IO.File.Copy(path, backupPath, true)
-                print notes
+                notesPrint notes
                 |> sprintf "\n***\n%s"
                 |> uncurry System.IO.File.AppendAllText path
         )
         let backupPath = System.IO.Path.ChangeExtension(randomNotesPath, ".bak")
         System.IO.File.Copy(randomNotesPath, backupPath, true)
-        print randomNotes2
+        notesPrint randomNotes2
         |> uncurry System.IO.File.WriteAllText randomNotesPath
     else
         failwith "some file not exists"
@@ -310,21 +313,15 @@ let sort path =
 
             notes
             |> List.sortBy (fun x -> x.DateTime |> Option.get)
-            |> print
+            |> notesPrint
             |> uncurry System.IO.File.WriteAllText path
     else
         failwithf "'%s' not found" path
-// sort @"E:\All2\Notes\Characters\someChar.txt"
 
 let dateConvert str =
     match run pdatetimeStrange str with
     | Success(x, _, _) -> sprintf "%A" x |> Clipboard.setText
     | Failure(x, _, _) -> failwithf "%s" x
-// dateConvert "‎26 ‎сентября ‎2017 ‎г., ‏‎3:44"
-// System.DateTime.Parse "32.05.2019 0:58:12" |> fun x -> x.ToString() |> Clipboard.setText
-// System.DateTime.Parse "19 июля 2019 г., 12:13" - System.DateTime.Parse "01.02.2020 16:35:00"
-// System.DateTime.Parse "‎26 ‎сентября ‎2017 ‎г., ‏‎3:44" |> fun x -> x.ToString() |> Clipboard.setText
-// System.DateTime.Now - System.TimeSpan(1, 7, 0) |> fun x -> x.ToString()
 
 let putDateTimeAtBeginAndEnd path =
     let file = System.IO.FileInfo path
@@ -336,7 +333,14 @@ let putDateTimeAtBeginAndEnd path =
         yield sprintf "// %A" file.LastWriteTime
     |]
     |> uncurry System.IO.File.WriteAllLines path
-
+let addTagsUniq xs note =
+    { note with
+        Tags =
+            xs
+            |> List.fold
+                (fun st x -> Set.add x st)
+                (Set.ofList note.Tags)
+            |> Set.toList }
 // #load @"CoreDiary.fs"
 module DiaryProc =
     open CoreDiary
@@ -354,7 +358,7 @@ module DiaryProc =
         let filter () =
             xs
             |> Seq.filter (fun (_, note) ->
-                System.Text.RegularExpressions.Regex.IsMatch(note, "почт")
+                System.Text.RegularExpressions.Regex.IsMatch(note, "собес")
                 )
             |> Seq.map (sprintf "%A")
             |> fun cont -> System.IO.File.WriteAllLines ("output\\output.txt", cont)
@@ -379,3 +383,120 @@ module DiaryProc =
         // let db:CoreDiary.BD = Json.desf @"e:\All2\Projects\Diary\GUI\bin\Debug\dataBase.db"
 
         ()
+
+open FParsec
+let datetimeFileFormat (d:System.DateTime) =
+    d.ToString("yyyy-MM-dd_HH-mm-ss")
+assert
+    (System.DateTime.Parse "18.11.2020 13:12:22" |> datetimeFileFormat = "2020-11-18_13-12-22")
+
+let toNewWorldOrder notesDir notesPath =
+    let notes = startOnFile notesPath
+
+    let validNotes, restNotes =
+        notes |> List.partition (fun note -> Option.isSome note.DateTime)
+
+    let validNotesPaths =
+        validNotes
+        |> List.map (fun note ->
+            let path =
+                let dt = note.DateTime.Value
+                datetimeFileFormat dt
+                |> sprintf "%s.md"
+                |> fun fileName -> System.IO.Path.Combine(notesDir, fileName)
+            if System.IO.File.Exists path then
+                failwithf "%A already exists" path
+            path, note
+        )
+
+    validNotesPaths
+    |> List.iter (fun (path, note) ->
+        System.IO.File.WriteAllText(path, notePrint note)
+    )
+    System.IO.File.Copy(notesPath, backupPath notesPath, true)
+    notesPrint restNotes
+    |> uncurry System.IO.File.WriteAllText notesPath
+
+
+let getShortDscr (note:Note) =
+    let dscr = note.Text.Replace("\n", "\\n")
+    let length = 100
+    if dscr.Length < length then
+        dscr
+    else
+        sprintf "%s..." dscr.[..length - 1]
+
+let createTagsMap notesDir =
+    let notesPaths = System.IO.Directory.EnumerateFiles notesDir
+
+    let tagNotes =
+        notesPaths
+        |> Seq.map (fun notePath ->
+            parseNoteOnFile notePath
+            |> Either.map (fun note -> notePath, note)
+        )
+        |> Seq.seqEither
+        |> Either.map (fun notes ->
+            notes
+            |> List.fold (fun st (notePath, note) ->
+                let shortDscr = getShortDscr note
+                note.Tags
+                |> List.fold (fun st tag ->
+                    st
+                    |> Map.addOrMod tag [notePath, shortDscr] (fun x -> (notePath, shortDscr)::x)
+                ) st
+            ) Map.empty
+        )
+    tagNotes
+    |> Either.map
+        (Seq.map (fun (KeyValue(tag, notePaths)) ->
+            notePaths
+            |> List.rev
+            |> List.map (fun (notePath, shortDescr) ->
+                let noteUri = System.Uri notePath |> fun x -> x.AbsoluteUri
+                sprintf "    * [%s](%s)" shortDescr noteUri)
+            |> String.concat "\n"
+            |> sprintf "* %s\n%s" tag
+        )
+        >> String.concat "\n"
+    )
+
+let withoutTags notesDir =
+    let notesPaths = System.IO.Directory.EnumerateFiles notesDir
+
+    let tagNotes =
+        notesPaths
+        |> Seq.map (fun notePath ->
+            parseNoteOnFile notePath
+            |> Either.map (fun note -> notePath, note)
+        )
+        |> Seq.seqEither
+        |> Either.map (fun notes ->
+            notes
+            |> List.choose (fun (notePath, note) ->
+                if List.isEmpty note.Tags then
+                    Some(notePath, getShortDscr note)
+                else None
+            )
+        )
+    tagNotes
+    |> Either.map (
+        List.map (fun (notePath, noteShortDscr) ->
+            let noteUri = System.Uri notePath |> fun x -> x.AbsoluteUri
+            sprintf "* [%s](%s)" noteShortDscr noteUri)
+        >> String.concat "\n"
+    )
+
+let modifyNotes fn notesPath =
+    let notes = startOnFile notesPath
+    let notes =
+        notes |> List.map fn
+
+    System.IO.File.Copy(notesPath, backupPath notesPath, true)
+    notesPrint notes
+    |> uncurry System.IO.File.WriteAllText notesPath
+
+let tagEscape (str:string) =
+    str
+    |> fun x -> x.Trim()
+    |> fun x -> x.Replace(" ", "_")
