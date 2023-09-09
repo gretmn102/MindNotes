9c5a88309700733322b61845c1e9502138d60d60
gretmn102 <erlgreywatson@gmail.com>
2023-09-10 00:31:58 +0300

    server: refact: remove Note.fs

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -478,11 +478,10 @@ let webApp =
     |> Remoting.fromValue api
     |> Remoting.buildHttpHandler
 
-let combinedApi = Giraffe.Core.choose [ webApp; Note.myApis; ]
 let app =
     application {
         url "http://0.0.0.0:8085"
-        use_router combinedApi
+        use_router webApp
         memory_cache
         use_static "public"
         use_gzip
